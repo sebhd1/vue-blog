b83133c36c258dd62bcb2206011c673a956a59be
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-23 18:44:24 +0200

    add logout/login functionalities and set up loggedUser as with provide

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,55 @@
 <script setup>
-import { ref } from 'vue';
-import Navigation from './components/partials/Navigation.vue';
+    import { ref, provide, readonly } from 'vue';
+    import Navigation from './components/partials/Navigation.vue';
 
-const registeredUsers = ref ([]);
+    const loggedUser = ref(null);
 
-function createUser(user){
-    registeredUsers.value.push({
-        id: crypto.randomUUID(),
-        ... user
-    })
-}
+    function logout() {
+        loggedUser.value = null;
+    }
 
+    provide('user', {
+        user: readonly(loggedUser),
+        logout,
+    });
+
+
+    const registeredUsers = ref ([]);
+
+
+    function createUser(user){
+        registeredUsers.value.push({
+            id: crypto.randomUUID(),
+            ... user
+        })
+    }
+
+    function login(user) {
+
+        registeredUsers.value.forEach(registeredUser => {
+            if (registeredUser.email === user.email && registeredUser.password === user.password) {
+                loggedUser.value = user.value;
+            }
+        });
+
+        if (loggedUser.value) {
+            console.log('Logged in successfully!');
+        } else {
+            console.log('Login failed');
+        }
+    }
 </script>
 
 <template>
 
     <Navigation/>
 
-    <RouterView />
+    <RouterView
+        @CreateUser="createUser"
+
+        @login="login"
+
+    />
 </template>
 
 <style>
