211eb6a994ab7d57ae8bd52e188422fb60743667
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-23 15:18:34 +0200

    imported router in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from './routes.js';
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+
+createApp(App)
+    .use(router)
+    .mount('#app')
