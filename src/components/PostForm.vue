41ff3f9a4c780d066508670282907ec3e785cc7f
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-24 17:41:35 +0200

    created Posts related components

---
+++ src/components/PostForm.vue
@@ -0,0 +1,46 @@
+<script setup>
+    //title (`string`),
+    // slug (`string`),
+    // content (`string`),
+    // author {id: `string(UUID)`,
+    // name: `string`}
+    // and comment (`string`).
+
+    import { ref } from 'vue';
+
+    const post = ref({
+        title: null,
+        slug: null,
+        content: null,
+    });
+
+</script>
+
+<template>
+
+    <form @submit.prevent="$emit('')">
+        <div>
+            <label for="title">title: </label>
+            <input v-model="post.title" type="text" id="title" placeholder="E.g A beautiful.." required>
+        </div>
+
+        <div>
+            <label for="name">slug: </label>
+            <input v-model.trim="post.slug" type="text" id="slug" placeholder="E.g A beautiful.." required>
+        </div>
+
+        <div>
+            <label for="content">content: </label>
+            <textarea v-model="post.title" id="content" placeholder="E.g A beautiful swoosh.." ></textarea>
+        </div>
+
+
+        <button type="submit">x</button>
+    </form>
+
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PostIndex.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import Post from './Post.vue';
+import { ref } from 'vue';
+
+const posts = ref([]);
+</script>
+
+<template>
+
+    <div v-for="post in posts">
+
+        <Post/>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
