ab6f6b51b5955783c7007634b7bc51bc93a2eec3
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-24 21:55:05 +0200

    defined new routes for logout, post creation and post lists

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,20 @@ import Home from './components/Home.vue';
 import Login from './components/Login.vue';
 import Register from './components/Register.vue';
 import PostIndex from './components/PostIndex.vue';
-import Post from './components/Post.vue';
 import PostForm from './components/PostForm.vue';
+import Logout from './components/Logout.vue';
+
 
 export default [
-    { path: '/', component: Home, name:'home'},
+    { path: '/', component: Home, name: 'home'},
     { path: '/login', component: Login, name:'login'},
-    { path: '/register', component: Register, name:'register'},
-    { path: '/posts', component: PostIndex, children: [] },
-    { path: '/post/creation', component: PostForm, name:'post-create'},
+    { path: '/logout', component: Logout, name: 'logout'},
+    { path: '/register', component: Register, name: 'register'},
+    { path: '/posts', component: PostIndex, name:'posts'},
+    { path: '/post/creation', component: PostForm, name: 'post-create'},
+   /* { path: '/posts', component: PostIndex, children: [] },
     { path: '/post/edit', component: PostForm, name:'post-edit'},
-    { path: '/posts/post', component: Post, name:'post'},
+    ,*/
 ]
 
 //{ path: '/post/edit/:id', component: PostForm, name:'post-edit'},
