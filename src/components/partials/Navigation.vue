08039006a9067b42d019ac1ccbd73fd44edb1aca
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-24 21:57:17 +0200

    defined new router links for navigation

--- src/components/partials/Navigation.vue
+++ src/components/partials/Navigation.vue
@@ -1,19 +1,27 @@
 <script setup>
+    import { inject } from 'vue';
+
+    const {user, logout} = inject('user');
 
 </script>
 
 <template>
    <nav>
+
        <RouterLink :to="{name:'home'}">Go to Home</RouterLink>
-       <RouterLink :to="{name:'login'}">Login</RouterLink>
-       <RouterLink :to="{name:'register'}">Register</RouterLink>
 
-       <RouterLink :to="{name:'post'}">Post</RouterLink>
+       <template v-if="user">
+           <RouterLink :to="{name:'posts'}">post list</RouterLink>
+           <RouterLink :to="{name:'post-create'}">create a post</RouterLink>
+           <router-link :to="{name: 'logout'}">Logout</router-link>
+           <!--<RouterLink :to="{name:'post'}">Post</RouterLink>
+       <RouterLink :to="{name:'post-edit'}">edit a post</RouterLink>-->
+       </template>
 
-       <RouterLink :to="{name:'posts'}">post list</RouterLink>
-
-       <RouterLink :to="{name:'post-create'}">create a post</RouterLink>
-       <RouterLink :to="{name:'post-edit'}">edit a post</RouterLink>
+       <template v-else>
+           <RouterLink :to="{name:'login'}">Login</RouterLink>
+           <RouterLink :to="{name:'register'}">Register</RouterLink>
+       </template>
    </nav>
 </template>
 
