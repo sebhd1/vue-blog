8d00d351c306ff0dd7537dfaaeb430cfa7c13163
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-26 12:40:48 +0200

    simplified login logic

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,45 @@
 <script setup>
     import { ref, provide, readonly } from 'vue';
-    import Navigation from './components/partials/Navigation.vue';
+    import Navigation from './components/Navigation.vue';
     import { useRouter } from 'vue-router';
 
     const router = useRouter();
 
     const loggedUser = ref(null);
-    const registeredUsers = ref ([]);
-    const postLists = ref([]);
+    const registeredUsers = ref([
 
-    provide('posts', {
-        posts: postLists,
-    });
+        {
+            id: crypto.randomUUID(),
+            name: 'seb',
+            email: 'admin1@admin.com',
+            password: '123'
+        }
+    ]);
 
-    provide('user', {
-        user: readonly(loggedUser),
-        logout,
-    });
-
-    function createUser(user){
+    function createUser(user) {
         registeredUsers.value.push({
             id: crypto.randomUUID(),
-            ... user
+            ...user
         });
     }
 
     function login(user) {
         console.log('Attempting login for user:', user);
-        registeredUsers.value.forEach(registeredUser => {
-            if (registeredUser.email === user.email && registeredUser.password === user.password) {
-                loggedUser.value = user;
-                console.log('Logged in successfully!', user);
-            }
+        const userFound = registeredUsers.value.find(registeredUser => {
+            return registeredUser.email === user.email &&
+                registeredUser.password === user.password;
         });
+
+        if (userFound) {
+
+            loggedUser.value = {
+                id: userFound.id,
+                name: userFound.name,
+                email: userFound.email,
+            }
+
+            console.log('logged user', loggedUser.value)
+        }
 
         if (!loggedUser.value) {
             console.log('Login failed');
@@ -41,22 +48,23 @@
 
     function logout() {
         loggedUser.value = null;
-        router.push({name: 'home'})
+        router.push({ name: 'home' })
     }
 
-    function createPost(post) {
-        postLists.value.push(post);
-    }
+    provide('user', {
+        user: readonly(loggedUser),
+        logout,
+    });
+
 </script>
 
 <template>
 
-    <Navigation/>
+    <Navigation />
 
     <RouterView
-        @CreateUser="createUser"
+        @createUser="createUser"
         @login="login"
-        @createPost="createPost"
     />
 </template>
 
