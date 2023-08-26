f483fd60a773c0c37c26518d1be5c60bcc915b8f
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-26 12:39:02 +0200

    created postview component to serve post logic

---
+++ src/views/PostView.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import { provide, ref } from 'vue';
+
+    const postsList = ref([]);
+
+    provide('posts', postsList);
+    function createPost(post) {
+        postsList.value.push({
+            id: crypto.randomUUID(),
+            ...post
+        });
+    }
+</script>
+
+
+<template>
+
+    <RouterView
+        @createPost="createPost"
+    />
+
+</template>
+
+<style scoped>
+
+</style>
