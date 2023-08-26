8278b08811676fee8baf184cb47bbbdfa805c141
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-26 12:38:24 +0200

    add nested routes for handling the post logic.

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,44 @@
 import Home from './components/Home.vue';
 import Login from './components/Login.vue';
 import Register from './components/Register.vue';
-import PostIndex from './components/PostIndex.vue';
 import PostForm from './components/PostForm.vue';
 import Logout from './components/Logout.vue';
+import PostView from './views/PostView.vue';
+import Post from './components/Post.vue';
+import PostList from './components/PostList.vue';
 
 
 export default [
-    { path: '/', component: Home, name: 'home'},
-    { path: '/login', component: Login, name:'login'},
-    { path: '/logout', component: Logout, name: 'logout'},
-    { path: '/register', component: Register, name: 'register'},
-    { path: '/posts', component: PostIndex, name:'posts'},
-    { path: '/post/creation', component: PostForm, name: 'post-create'},
-   /* { path: '/posts', component: PostIndex, children: [] },
-    { path: '/post/edit', component: PostForm, name:'post-edit'},
-    ,*/
+    { path: '/', component: Home, name: 'home' },
+    { path: '/login', component: Login, name: 'login' },
+    { path: '/logout', component: Logout, name: 'logout' },
+    { path: '/register', component: Register, name: 'register' },
+    {
+        path: '/posts', component: PostView, name: 'posts', children: [
+            {
+                path: '',
+                component: PostList,
+                name: 'posts.index',
+            },
+
+            {
+                path: 'create',
+                component: PostForm,
+                name: 'posts.create',
+            },
+
+            {
+                path: ':id',
+                component: Post,
+                name: 'posts.show',
+            }
+        ]
+    },
 ]
+
+/* { path: '/posts', component: PostIndex, children: [] },
+   { path: '/post/edit', component: PostForm, name:'post-edit'},
+   ,*/
 
 //{ path: '/post/edit/:id', component: PostForm, name:'post-edit'},
 //     { path: '/posts/:id', component: Post, name:'post'},
