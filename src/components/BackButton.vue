ec0b7560f52dab0637216b9ef0e738d5f02211c8
Sebastiano Ballotta <sebeballotta@outlook.com>
2023-08-26 12:38:09 +0200

    created a reusable BackButton.vue component

---
+++ src/components/BackButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+
+</script>
+
+<template>
+
+    <button type="button" @click="router.back()">Go back</button>
+
+</template>
+
+<style scoped>
+
+</style>
